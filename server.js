b3f5832a9455c82b085a9f2eb7606827818a3bb0
skpatel05 <sharadkpatel05@gmail.com>
2024-12-19 15:28:20 +0530

    add cors

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({
-    origin: 'http://localhost:5173',  // Allow only this origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // You can specify allowed methods
-    credentials: true,  // Allow cookies to be sent with the request (optional)
-  }));
+app.use(cors({ origin: 'https://spotify-fe-two.vercel.app' }));
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
